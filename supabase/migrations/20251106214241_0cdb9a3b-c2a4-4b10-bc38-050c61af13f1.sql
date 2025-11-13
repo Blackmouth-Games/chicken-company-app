4bd368e283a1424984b561d64c2911a4e1392fd8
Carlos Peralta <carlos@blackmouthgames.com>
2025-11-13 13:20:07 +0100

    [remove] migrations files not necesary for the future

--- supabase/migrations/20251106214241_0cdb9a3b-c2a4-4b10-bc38-050c61af13f1.sql
+++
@@ -1,5 +0,0 @@
--- Add new metric types for building purchases
-ALTER TYPE metric_type ADD VALUE IF NOT EXISTS 'building_purchased';
-ALTER TYPE metric_type ADD VALUE IF NOT EXISTS 'ton_payment_initiated';
-ALTER TYPE metric_type ADD VALUE IF NOT EXISTS 'ton_payment_completed';
-ALTER TYPE metric_type ADD VALUE IF NOT EXISTS 'building_upgraded';
